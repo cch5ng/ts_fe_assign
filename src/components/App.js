50e4c33ef874a998b458766b21d17958f911bc47
Carol Chung <cch5ng@gmail.com>
2021-07-01 11:16:41 -0700

    start on api fetch

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,39 @@ import logo from '../images/logo.svg';
 import searchIcon from '../images/search-icon.svg';
 import './App.css';
 
-const App = () => (
-	<div className="app">
-		<header>
-			<div>
-				<img src={logo} alt="Timescale" />
-			</div>
+const App = () => {
+	const [movies, setMovies] = useState([]);
 
-			<div className="input-icons">
-            <img src={searchIcon} className="icon" />
-            <input className="input-field" type="text" placeholder="Search for a movie"/>
-			</div>
-		</header>
-		<main>
+	useEffect(() => {
 
-		</main>
-	</div>
-)
+//sample url
+//
+		const url = `https://api.themoviedb.org/3/discover/movie?api_key={process.env.REACT_APP_MOVIE_DB_API_KEY}&language=de-DE&region=DE&release_date.gte=2016-11-16&release_date.lte=2016-12-02&with_release_type=2|3`;
+
+
+	}, []);
+
+	return (
+		<div className="app">
+			<header>
+				<div>
+					<img src={logo} alt="Timescale" />
+				</div>
+	
+				<div className="input-icons">
+							<img src={searchIcon} className="icon" />
+							<input className="input-field" type="text" placeholder="Search for a movie"/>
+				</div>
+			</header>
+			<main>
+				<h1>Most Recent Movies</h1>
+				<div>
+	
+				</div>
+	
+			</main>
+		</div>
+	)
+}
 
 export default App;
