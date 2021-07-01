2ea35a01b523882e215487b0d148b3dea7b4635d
Carol Chung <cch5ng@gmail.com>
2021-06-30 22:19:40 -0700

    stub header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react';
+
 import logo from '../images/logo.svg';
+import './App.css';
 
 const App = () => (
-	<img src={logo} alt="Timescale" />
+	<div className="app">
+		<header>
+			<div>
+				<img src={logo} alt="Timescale" />
+			</div>
+			<div>
+				<input type="text" />
+			</div>
+		</header>
+		<main>
+
+		</main>
+	</div>
 )
 
 export default App;
