58ccde78567f8639b23c7c6222cd08256b3b50cd
Carol Chung <cch5ng@gmail.com>
2021-07-01 13:30:40 -0700

    stub movie layout

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,13 @@
-const Movie = () => {
+import './Movie.css';
+
+const Movie = ({ movie }) => {
+  const imgUrl = `${process.env.REACT_APP_API_BASE_IMAGE_URL}${movie.poster_path}`;
   return (
-    <div></div>
+    <div className="movie_container">
+      <div className="vote">{movie.vote_average}</div>
+      <div className="poster_container"><img className="poster" src={imgUrl} alt="poster image" /></div>
+      <div className="title">{movie.title}</div>
+    </div>
   )
 }
 
