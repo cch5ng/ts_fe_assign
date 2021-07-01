f552cfcba1b9e7a3281f2f991d02e7c92880985d
Carol Chung <cch5ng@gmail.com>
2021-07-01 15:32:41 -0700

    init modal trigger

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,11 @@
 import './Movie.css';
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, handleModalOpen }) => {
   const imgUrl = `${process.env.REACT_APP_API_BASE_IMAGE_URL}${movie.poster_path}`;
   return (
-    <div className="movie_container">
+    <div className="movie_container" onClick={(ev) => handleModalOpen(ev)}>
       <div className="vote">{movie.vote_average}</div>
-      <div className="poster_container"><img className="poster" src={imgUrl} alt="poster image" /></div>
+      <div className="poster_container"><img id={movie.id} className="poster" src={imgUrl} alt="poster image" /></div>
       <div className="title">{movie.title}</div>
     </div>
   )
